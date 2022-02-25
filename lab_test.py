d77625fcdfc41f6285e953d0985034a255cd3aca
DeleMike <akindelemichael65@gmail.com>
2022-02-25 11:43:43 +0100

    create test_lab

---
+++ lab_test.py
@@ -0,0 +1,14 @@
+game = [
+  [["wall"], ["wall"], ["wall"], ["wall"], ["wall"], ["wall"]],
+  [["wall"], [], [], [], [], ["wall"]],
+  [["wall"], [], ["wall"], ["player"], [], ["wall"]],
+  [["wall"], [], ["computer"], ["target", "computer"], [], ["wall"]],
+  [["wall"], [], ["target"], ["target", "computer"], [], ["wall"]],
+  [["wall"], [], [], [], [], ["wall"]],
+  [["wall"], ["wall"], ["wall"], ["wall"], ["wall"], ["wall"]]
+]
+
+# internal representation
+
+for first_block in game:
+    print
